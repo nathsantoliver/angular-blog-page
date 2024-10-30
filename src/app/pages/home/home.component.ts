e8e8f2969db791c1b5d5c44cff24e6aea438abf7
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-10-29 22:56:36 -0300

    styling the cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { MainArticleComponent } from "../../components/main-article/main-article.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, MainArticleComponent],
+  imports: [NavbarComponent, MainArticleComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
