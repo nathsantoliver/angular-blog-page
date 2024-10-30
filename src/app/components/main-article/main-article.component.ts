90d6e0ee241094d65a5eb906482b23c8a14c5fe0
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-10-29 21:13:36 -0300

    create the main-article section

---
+++ src/app/components/main-article/main-article.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-article.component.html',
+  styleUrl: './main-article.component.css'
+})
+export class MainArticleComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { MainArticleComponent } from "../../components/main-article/main-article.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MainArticleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
