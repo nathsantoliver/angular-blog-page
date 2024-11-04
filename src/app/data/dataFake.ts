981efeb1e1623e0e75bf628fdc5c11c838fae91a
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-11-04 11:22:55 -0300

    data file finished

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,10 +1,10 @@
 export const dataFake = [
 	{
 		"id": "1",
-		"title": "saiu o novo filme",
+		"title": "Feynman Technique: what it is and how to use it to improve learning",
     "author": "sunny",
     "date": "2024.10.25",
-		"photoCover": "link",
+		"photoCover": "img/photo_1.jpg",
     "social": [
       { "name": "instagram", "url": "www.instagram.com" },
       { "name": "x-twitter", "url": "www.x.com" },
@@ -17,8 +17,99 @@ export const dataFake = [
 	},
 	{
 		"id": "2",
-		"title": "saiu outro filme",
-		"photoCover": "link"
+		"title": "Things that I cary on everyday",
+    "author": "sunny",
+    "date": "2024.10.21",
+		"photoCover": "img/photo_6.png",
+    "social": [
+      { "name": "instagram", "url": "www.instagram.com" },
+      { "name": "x-twitter", "url": "www.x.com" },
+      { "name": "youtube", "url": "www.youtube.com" }
+    ],
+    "content": {
+      "intro": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore velpossimus distinctio, incidunt praesentium amet eos illo illum porro velit! \nLorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatem numquam quis et, repudiandae, veniam, similique harum provident voluptatum est delectus consequuntur. Corporis, repudiandae? Distinctio consequuntur tempore illum sed praesentium.",
+      "body": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus est harum ducimus asperiores eligendi accusamus, ab voluptates illo illum! Suscipit quisquam magni, quos neque, nulla, eveniet doloribus facere sit veniam omnis labore est iusto voluptate? Excepturi natus qui sed aut sit animi illo asperiores cum repellendus recusandae. Quo, soluta sequi.\nLorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat hic, doloremque voluptatibus accusamus ad assumenda ratione vitae minus? Autem tempore similique provident alias facilis delectus eum ipsa consectetur totam excepturi nihil, quibusdam laboriosam architecto nobis accusamus quae molestias nisi! Ut ad ducimus molestias, exercitationem cupiditate voluptas velit quisquam eligendi corrupti, quos expedita. Dolorum aliquid modi, veritatis in incidunt, autem sapiente sint temporibus pariatur ipsam voluptatum consectetur qui labore non itaque obcaecati mollitia, cum nemo! Ea nemo quidem dolorum iste corporis explicabo molestias ratione modi nulla facere architecto porro perferendis, esse fugit rem, magni blanditiis vero dicta commodi, officia recusandae. Doloribus.",
+    }
+	},
+	{
+		"id": "3",
+		"title": "Everything you need to know before getting a Shi-Tzu",
+    "author": "sunny",
+    "date": "2024.10.18",
+		"photoCover": "img/photo_3.jpg",
+    "social": [
+      { "name": "instagram", "url": "www.instagram.com" },
+      { "name": "facebook", "url": "www.facebook.com" },
+      { "name": "youtube", "url": "www.youtube.com" }
+    ],
+    "content": {
+      "intro": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore velpossimus distinctio, incidunt praesentium amet eos illo illum porro velit! \nLorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatem numquam quis et, repudiandae, veniam, similique harum provident voluptatum est delectus consequuntur. Corporis, repudiandae? Distinctio consequuntur tempore illum sed praesentium.",
+      "body": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus est harum ducimus asperiores eligendi accusamus, ab voluptates illo illum! Suscipit quisquam magni, quos neque, nulla, eveniet doloribus facere sit veniam omnis labore est iusto voluptate? Excepturi natus qui sed aut sit animi illo asperiores cum repellendus recusandae. Quo, soluta sequi.\nLorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat hic, doloremque voluptatibus accusamus ad assumenda ratione vitae minus? Autem tempore similique provident alias facilis delectus eum ipsa consectetur totam excepturi nihil, quibusdam laboriosam architecto nobis accusamus quae molestias nisi! Ut ad ducimus molestias, exercitationem cupiditate voluptas velit quisquam eligendi corrupti, quos expedita. Dolorum aliquid modi, veritatis in incidunt, autem sapiente sint temporibus pariatur ipsam voluptatum consectetur qui labore non itaque obcaecati mollitia, cum nemo! Ea nemo quidem dolorum iste corporis explicabo molestias ratione modi nulla facere architecto porro perferendis, esse fugit rem, magni blanditiis vero dicta commodi, officia recusandae. Doloribus.",
+    }
+	},
+	{
+		"id": "4",
+		"title": "How I organize my study and work routine to be more productive",
+    "author": "sunny",
+    "date": "2024.10.15",
+		"photoCover": "img/photo_4.jpg",
+    "social": [
+      { "name": "instagram", "url": "www.instagram.com" },
+      { "name": "linkedin", "url": "www.linkedin.com" },
+      { "name": "youtube", "url": "www.youtube.com" }
+    ],
+    "content": {
+      "intro": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore velpossimus distinctio, incidunt praesentium amet eos illo illum porro velit! \nLorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatem numquam quis et, repudiandae, veniam, similique harum provident voluptatum est delectus consequuntur. Corporis, repudiandae? Distinctio consequuntur tempore illum sed praesentium.",
+      "body": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus est harum ducimus asperiores eligendi accusamus, ab voluptates illo illum! Suscipit quisquam magni, quos neque, nulla, eveniet doloribus facere sit veniam omnis labore est iusto voluptate? Excepturi natus qui sed aut sit animi illo asperiores cum repellendus recusandae. Quo, soluta sequi.\nLorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat hic, doloremque voluptatibus accusamus ad assumenda ratione vitae minus? Autem tempore similique provident alias facilis delectus eum ipsa consectetur totam excepturi nihil, quibusdam laboriosam architecto nobis accusamus quae molestias nisi! Ut ad ducimus molestias, exercitationem cupiditate voluptas velit quisquam eligendi corrupti, quos expedita. Dolorum aliquid modi, veritatis in incidunt, autem sapiente sint temporibus pariatur ipsam voluptatum consectetur qui labore non itaque obcaecati mollitia, cum nemo! Ea nemo quidem dolorum iste corporis explicabo molestias ratione modi nulla facere architecto porro perferendis, esse fugit rem, magni blanditiis vero dicta commodi, officia recusandae. Doloribus.",
+    }
+	},
+	{
+		"id": "5",
+		"title": "Why keeping a diary helps you build and maintain good habits",
+    "author": "sunny",
+    "date": "2024.10.12",
+		"photoCover": "img/photo_5.jpg",
+    "social": [
+      { "name": "instagram", "url": "www.instagram.com" },
+      { "name": "x-twitter", "url": "www.x.com" },
+      { "name": "youtube", "url": "www.youtube.com" }
+    ],
+    "content": {
+      "intro": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore velpossimus distinctio, incidunt praesentium amet eos illo illum porro velit! \nLorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatem numquam quis et, repudiandae, veniam, similique harum provident voluptatum est delectus consequuntur. Corporis, repudiandae? Distinctio consequuntur tempore illum sed praesentium.",
+      "body": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus est harum ducimus asperiores eligendi accusamus, ab voluptates illo illum! Suscipit quisquam magni, quos neque, nulla, eveniet doloribus facere sit veniam omnis labore est iusto voluptate? Excepturi natus qui sed aut sit animi illo asperiores cum repellendus recusandae. Quo, soluta sequi.\nLorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat hic, doloremque voluptatibus accusamus ad assumenda ratione vitae minus? Autem tempore similique provident alias facilis delectus eum ipsa consectetur totam excepturi nihil, quibusdam laboriosam architecto nobis accusamus quae molestias nisi! Ut ad ducimus molestias, exercitationem cupiditate voluptas velit quisquam eligendi corrupti, quos expedita. Dolorum aliquid modi, veritatis in incidunt, autem sapiente sint temporibus pariatur ipsam voluptatum consectetur qui labore non itaque obcaecati mollitia, cum nemo! Ea nemo quidem dolorum iste corporis explicabo molestias ratione modi nulla facere architecto porro perferendis, esse fugit rem, magni blanditiis vero dicta commodi, officia recusandae. Doloribus.",
+    }
+	},
+	{
+		"id": "6",
+		"title": "Books that changed my mindset for life",
+    "author": "sunny",
+    "date": "2024.10.09",
+		"photoCover": "img/photo_2.jpg",
+    "social": [
+      { "name": "instagram", "url": "www.instagram.com" },
+      { "name": "x-twitter", "url": "www.x.com" },
+      { "name": "youtube", "url": "www.youtube.com" }
+    ],
+    "content": {
+      "intro": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore velpossimus distinctio, incidunt praesentium amet eos illo illum porro velit! \nLorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatem numquam quis et, repudiandae, veniam, similique harum provident voluptatum est delectus consequuntur. Corporis, repudiandae? Distinctio consequuntur tempore illum sed praesentium.",
+      "body": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus est harum ducimus asperiores eligendi accusamus, ab voluptates illo illum! Suscipit quisquam magni, quos neque, nulla, eveniet doloribus facere sit veniam omnis labore est iusto voluptate? Excepturi natus qui sed aut sit animi illo asperiores cum repellendus recusandae. Quo, soluta sequi.\nLorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat hic, doloremque voluptatibus accusamus ad assumenda ratione vitae minus? Autem tempore similique provident alias facilis delectus eum ipsa consectetur totam excepturi nihil, quibusdam laboriosam architecto nobis accusamus quae molestias nisi! Ut ad ducimus molestias, exercitationem cupiditate voluptas velit quisquam eligendi corrupti, quos expedita. Dolorum aliquid modi, veritatis in incidunt, autem sapiente sint temporibus pariatur ipsam voluptatum consectetur qui labore non itaque obcaecati mollitia, cum nemo! Ea nemo quidem dolorum iste corporis explicabo molestias ratione modi nulla facere architecto porro perferendis, esse fugit rem, magni blanditiis vero dicta commodi, officia recusandae. Doloribus.",
+    }
+	},
+	{
+		"id": "7",
+		"title": "Best techniques for being a self-taught person",
+    "author": "sunny",
+    "date": "2024.10.06",
+		"photoCover": "img/photo_1.jpg",
+    "social": [
+      { "name": "instagram", "url": "www.instagram.com" },
+      { "name": "x-twitter", "url": "www.x.com" },
+      { "name": "youtube", "url": "www.youtube.com" }
+    ],
+    "content": {
+      "intro": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore velpossimus distinctio, incidunt praesentium amet eos illo illum porro velit! \nLorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatem numquam quis et, repudiandae, veniam, similique harum provident voluptatum est delectus consequuntur. Corporis, repudiandae? Distinctio consequuntur tempore illum sed praesentium.",
+      "body": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus est harum ducimus asperiores eligendi accusamus, ab voluptates illo illum! Suscipit quisquam magni, quos neque, nulla, eveniet doloribus facere sit veniam omnis labore est iusto voluptate? Excepturi natus qui sed aut sit animi illo asperiores cum repellendus recusandae. Quo, soluta sequi.\nLorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat hic, doloremque voluptatibus accusamus ad assumenda ratione vitae minus? Autem tempore similique provident alias facilis delectus eum ipsa consectetur totam excepturi nihil, quibusdam laboriosam architecto nobis accusamus quae molestias nisi! Ut ad ducimus molestias, exercitationem cupiditate voluptas velit quisquam eligendi corrupti, quos expedita. Dolorum aliquid modi, veritatis in incidunt, autem sapiente sint temporibus pariatur ipsam voluptatum consectetur qui labore non itaque obcaecati mollitia, cum nemo! Ea nemo quidem dolorum iste corporis explicabo molestias ratione modi nulla facere architecto porro perferendis, esse fugit rem, magni blanditiis vero dicta commodi, officia recusandae. Doloribus.",
+    }
 	}
 ];
 
